afab6c2d2174bb12dfe36cabc761c3ea70341ae4
bruhmomentum21 <63405654+bruhmomentum21@users.noreply.github.com>
2020-09-07 15:55:47 +0800

    Update and rename main - Copy.py to main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ from pathlib import Path
 
 
 p = Path('.')
-
-call = ['based,','cringe!','tututut']
+call = ['based.','cringe! you\'re going to lose subscriber!']
 client = commands.Bot(command_prefix='!')
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -25,7 +25,7 @@ async def on_message(message):
 
 @client.event
 async def on_message_delete(message):
-    await message.channel.send(f'{message.author.mention}: said "{message.content}" in #{message.channel}. deleted at {message.channel} ')
+    await message.channel.send(f'{message.author.mention}: said "{message.content}" in #{message.channel}. and it got deleted ')
 
 @client.command(name='list_members')
 async def reaction(ctx: discord.message):
@@ -41,12 +41,11 @@ async def based(ctx):
     await asyncio.sleep(3)
     await edit.edit(content = call[random.randint(0,1)])
 
-
+#!attach [image] to upload images to pc
 @client.command()
 async def attach(ctx):
-
     f = ctx.message.attachments[0]
-
+    
     if f.url.endswith(".png") or f.url.endswith(".jpg"):
         with open(f'{uuid.uuid4()}.jpg', 'wb') as handle:
             response = requests.get(f.url, stream=True)
@@ -58,18 +57,21 @@ async def attach(ctx):
     else:
         await ctx.message.channel.send('format not supported!')
         await ctx.message.channel.send(':rage: :rage:')
-
+#sends images from pc       
+@client.command()
+async def show(ctx):
+    sans = list(p.glob('*.jpg'))
+    leng = len(sans)
+    rando = random.randint(0, leng)
+    await ctx.channel.send(file=discord.File(sans[rando]))
+    
 @client.command()
 @has_permissions(kick_members=True)
 async def kick(ctx, member: discord.Member, *, reason = None):
     amount = 2
     user = ctx.author
-
-    if reason == '':
-            ctx.message.channel.send(member + ' you need to put a reason')
-    else:
-        shrek = await member.kick(reason=reason)
-        await ctx.message.channel.send(f'{member} has been kicked by {user}')
+    await member.kick(reason=reason)
+    await ctx.message.channel.send(f'{member} has been kicked by {user}')
 
 @kick.error
 async def kick_error(ctx, error : MissingPermissions):
@@ -81,5 +83,5 @@ async def kick_error(ctx, error : MissingPermissions):
 
 
 
-client.run('client token here')
+client.run('Token discord file')
 
